aa9726b5dc70a82ff9b3ea154f26c060a699ff34
Kevin C Malin <racmalin@yahoo.com>
2020-04-18 23:50:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,5 @@
-def speak_to_grandma
-
-# speak_to_grandma("I LOVE YOU TOO PUMPKIN")
-
-if phrase == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif 'Hi Nana, how are you?' 
   puts "HUH?! SPEAK UP, SONNY!"

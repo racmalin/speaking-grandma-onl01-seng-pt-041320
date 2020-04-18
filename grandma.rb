c8efc3b92d8dbab29144cd7308d84054b9f88b97
Kevin C Malin <racmalin@yahoo.com>
2020-04-18 23:55:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,14 @@
-def speak_to_grandma(phrase)
+def  speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif 'Hi Nana, how are you?' 
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif 'Hi!'  
-  puts "HUH?! SPEAK UP, SONNY"
-else  "WHAT DID YOU EAT TODAY?"
-  puts "NO, NOT SINCE 1938!"
-else "WHAT?!"  
-  puts "NO, NOT SINCE 1938!"
+   "I LOVE YOU TOO PUMPKIN!"
+  elsif 'Hi Nana, how are you?' 
+   "HUH?! SPEAK UP, SONNY!"
+  elsif 'Hi!'  
+   "HUH?! SPEAK UP, SONNY"
+  else  "WHAT DID YOU EAT TODAY?"
+   "NO, NOT SINCE 1938!"
+  else "WHAT?!"  
+   "NO, NOT SINCE 1938!"
  
 end
 

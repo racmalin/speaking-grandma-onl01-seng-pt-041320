d00e0f5eb29d9e7fa0d46dac6696a866dec74c87
Kevin C Malin <racmalin@yahoo.com>
2020-04-19 00:08:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else "NO, NOT SINCE 1938!" 
+  end  
 end
+
+speak_to_grandma("test")
